1d6977e227974fa8b3b0ad52aa0618f34f2adf99
Wei-Cheng Pan <legnaleurc@gmail.com>
2023-12-16 14:34:56 +0900

    add new command

--- wcpan/drive/cli/_cmd/remove.py
+++ wcpan/drive/cli/_cmd/remove.py
@@ -1,5 +1,6 @@
 from asyncio import as_completed
 from argparse import Namespace
+from functools import partial
 
 from wcpan.drive.core.types import Drive
 
@@ -16,10 +17,11 @@ def add_remove_command(commands: SubCommand):
     parser = commands.add_parser(
         "remove",
         aliases=["rm"],
-        help="trash files/folders",
+        help="remove files/folders",
     )
     add_bool_argument(parser, "restore")
-    parser.set_defaults(action=_action_remove, restore=False)
+    add_bool_argument(parser, "purge")
+    parser.set_defaults(action=_action_remove, restore=False, purge=False)
     parser.add_argument("id_or_path", type=str, nargs="+")
 
 
@@ -27,8 +29,20 @@ def add_remove_command(commands: SubCommand):
 async def _action_remove(drive: Drive, kwargs: Namespace) -> int:
     id_or_path: str = kwargs.id_or_path
     restore: bool = kwargs.restore
+    purge: bool = kwargs.purge
+
+    if restore and purge:
+        cerr("`--purge` flag conflicts with `--restore`")
+        return 1
+
+    action = (
+        partial(_purge_node, drive=drive)
+        if purge
+        else partial(_trash_node, drive=drive, trashed=not restore)
+    )
+
     rv = 0
-    for _ in as_completed(_trash_node(drive, _, not restore) for _ in id_or_path):
+    for _ in as_completed(action(_) for _ in id_or_path):
         try:
             await _
         except Exception:
@@ -45,6 +59,20 @@ async def _trash_node(drive: Drive, id_or_path: str, trashed: bool) -> None:
 
     try:
         await drive.move(node, trashed=trashed)
+    except Exception as e:
+        cerr(f"operation failed on {id_or_path}, reason: {str(e)}")
+        raise
+
+
+async def _purge_node(drive: Drive, id_or_path: str) -> None:
+    try:
+        node = await get_node_by_id_or_path(drive, id_or_path)
+    except Exception:
+        cerr(f"{id_or_path} does not exist")
+        raise
+
+    try:
+        await drive.delete(node)
     except Exception as e:
         cerr(f"operation failed on {id_or_path}, reason: {str(e)}")
         raise
