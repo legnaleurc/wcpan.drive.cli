f7cfce1d9231144ee875d361489c88916d262b83
Wei-Cheng Pan <legnaleurc@gmail.com>
2025-02-19 23:15:01 +0900

    fix: export name

--- wcpan/drive/cli/lib.py
+++ wcpan/drive/cli/lib.py
@@ -10,7 +10,7 @@ from ._cfg import create_drive_from_config as create_drive_from_config
 
 
 __all__ = (
-    "get_media_info",
+    "get_image_info",
     "get_video_info",
     "get_media_info",
     "get_file_hash",
